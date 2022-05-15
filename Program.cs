5b80149758b17ca7bcd30bbfaaaef8b44ce610c5
Furkhan Shaikh <53703371+29furkhan@users.noreply.github.com>
2022-05-15 16:32:03 +0530

    Add files via upload
    FileInfo class code

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace TSLP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "/Users/furkhan/Documents/index.txt";
+            FileInfo file = new FileInfo(path);
+
+            if (!file.Exists)
+                file.Create();
+            else
+            {
+                using (StreamWriter sw = file.CreateText())
+                {
+                    sw.WriteLine("Iron man");
+                    sw.WriteLine("Hulk");
+                    sw.WriteLine("Thor");
+                    sw.WriteLine("Captain america");
+                }
+
+                using (StreamReader sr = file.OpenText())
+                {
+                    string s = "";
+                    while((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+
+                Console.WriteLine(file.Length);
+                Console.WriteLine(file.Name);
+                Console.WriteLine(file.DirectoryName);
+
+                file.Delete();
+            }
+        }
+    }
+}
+
+/*
+Hulk
+Thor
+Captain america
+0
+index.txt
+/Users/furkhan/Documents
+*/
